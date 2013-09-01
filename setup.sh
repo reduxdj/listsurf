ff2c3d4e76ae58bfc15b1cbe218ecff06b9b4a28
Patrick Lemiuex <patricklemiuex@gmail.com>
2013-08-31 21:59:27 -0400

    fix for repo

--- setup.sh
+++ setup.sh
@@ -1,7 +1,6 @@
 heroku create --stack cedar --buildpack http://github.com/heroku/heroku-buildpack-c.git
-git push heroku master
-mkvirtualenv listsurf
-#deactivate
+heroku addons:add mongolab
 
-workon listsurf
-./listsurf.py
+git push heroku master
+echo 'export WORKON_HOME=~/environments' >> .bashrc # Change this directory if you don't like it
+echo '. /usr/local/bin/virtualenvwrapper.sh' >> .bashrc
