aef32dd6b8b11c61c87a2585de4db6b2e3e20c26
Patrick Lemiuex <patricklemiuex@gmail.com>
2013-09-15 21:43:00 -0400

    added switchout for schematics

--- models.py
+++ models.py
@@ -1,48 +1,71 @@
-from dictshield.document import Document, swap_field
-from dictshield.fields import (BaseField,
-                               StringField,
-                               BooleanField,
-                               URLField,
-                               EmailField,
-                               LongField)
-# this might get moved
-from dictshield.fields.mongo import ObjectIdField
-
-from brubeck.models import User
-from brubeck.timekeeping import MillisecondField
+from schematics.types import (
+    BaseType, StringType, DateTimeType, DateType, IntType, EmailType, LongType,
+    URLType, USDCurrencyType
+)
+from schematics.exceptions import ValidationError, StopValidation, ConversionError
 
 
-###
-### Social Models
-###
+"""This module contains fields that depend on importing `bson`. `bson` is
+as part of the pymongo distribution.
+"""
 
-### Adjust ID field in Brubeck models
-User = swap_field(User, ObjectIdField, ['id'])
+from schematics.types import BaseType
+from schematics.exceptions import ValidationError
 
+import bson
 
-###
-### List Models
-###
-    
-class ListItem(Document):
-    """Bare minimum to have the concept of streamed item.
+#Right now this is here because I can't seem to get it installed
+class ObjectIdType(BaseType):
+    """An field wrapper around MongoDB ObjectIds.  It is correct to say they're
+    bson fields, but I am unaware of bson being used outside MongoDB.
+
+    `auto_fill` is disabled by default for ObjectIdType's as they are
+    typically obtained after a successful save to Mongo.
     """
-    # ownable
-    owner = ObjectIdField(required=True)
-    username = StringField(max_length=30, required=True)
 
-    # streamable
-    created_at = MillisecondField()
-    updated_at = MillisecondField()
+    def __init__(self, auto_fill=False, **kwargs):
+        self.auto_fill = auto_fill
+        super(ObjectIdType, self).__init__(**kwargs)
 
-    url = URLField()
+    def convert(self, value):
+        if not isinstance(value, bson.objectid.ObjectId):
+            value = bson.objectid.ObjectId(unicode(value))
+        return value
 
-    class Meta:
-        id_field = ObjectIdField
+    def to_primitive(self, value):
+        return str(value)
 
-    _private_fields = [
-        'owner',
-    ]
+    def validate_id(self, value):
+        if not isinstance(value, bson.objectid.ObjectId):
+            try:
+                value = bson.objectid.ObjectId(unicode(value))
+            except Exception, e:
+                raise ValidationError('Invalid ObjectId')
+        return True
 
-    def __unicode__(self):
-        return u'%s' % (self.url)
+class User(User):
+  id = ObjectIdType()
+
+
+class ListItem(Model):
+  """Bare minimum to have the concept of streamed item.
+  """
+  owner = ObjectIdType(required=True)
+  username = StringType(max_length=30, required=True)
+  
+
+  # streamable
+  created_at = DateTimeType()
+  updated_at = DateTimeType()
+
+  url = URLType()
+
+  class Meta:
+      id_field = ObjectIdType
+
+  _private_fields = [
+      'owner',
+  ]
+
+  def __unicode__(self):
+      return u'%s' % (self.url)
